67ccdd3be73e33a55190ee82aa4fc958de7629a8
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-02-26 13:15:56 +1100

    Encapsulate in function

--- hasher.py
+++ hasher.py
@@ -1,10 +1,14 @@
 import hashlib
 
-raw_string = input("The password you want to protect: ")
+def hash_password():
+    raw_string = input("Enter your password-system password: ")
 
-# Because the Python hashlib library cannot hash unicode encoded strings,
-# such as those in utf-8, we need to first convert the string to bytes.
-# We can do this using the .encode() and .hexdigest() methods.
-hashed_string = hashlib.sha256( raw_string.encode('utf-8') ).hexdigest()
+    # Because the Python hashlib library cannot hash unicode encoded strings,
+    # such as those in utf-8, we need to first convert the string to bytes.
+    # We can do this using the .encode() and .hexdigest() methods.
+    hashed_string = hashlib.sha256( raw_string.encode('utf-8') ).hexdigest()
 
-print(hashed_string)
+    print("Please use the following as as your new secure password: ")
+    print(hashed_string)
+
+hash_password()

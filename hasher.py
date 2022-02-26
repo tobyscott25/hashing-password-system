d40717a712958892bfe7a1fbe9c2ce775c0a2c2f
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-02-26 13:44:35 +1100

    Remove every second character to distort the hash

--- hasher.py
+++ hasher.py
@@ -1,14 +1,21 @@
 import hashlib
 
 def hash_password():
-    raw_string = input("Enter your password-system password: ")
+    raw_password = input("Enter your password-system password: ")
 
     # Because the Python hashlib library cannot hash unicode encoded strings,
     # such as those in utf-8, we need to first convert the string to bytes.
     # We can do this using the .encode() and .hexdigest() methods.
-    hashed_string = hashlib.sha256( raw_string.encode('utf-8') ).hexdigest()
+    hash = hashlib.sha256( raw_password.encode('utf-8') ).hexdigest()
+    
+    # Convert hash to array of all characters
+    array = list(hash)
+    # Delete every second character from the array
+    del array[::2]
+    # Rejoin the array to a string
+    password = ''.join(array)
 
     print("Please use the following as as your new secure password: ")
-    print(hashed_string)
+    print(password)
 
 hash_password()
